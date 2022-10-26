6b9dd55bd7d72c857c04842de2f4bf0afd0cb8f7
shaman <shaman@shamans-MacBook-Pro.local>
2022-10-26 12:46:57 -0500

    Module-1 lesson02

---
+++ lesson02/script.js
@@ -0,0 +1,17 @@
+'use strict'
+{
+    const name = 'iPhone 12 Pro',
+        quantity = 5,
+        category = 'mobile phone',
+        price = 750;
+
+    let sum = quantity * price;
+    console.log(`Общая сумма товара ${sum}`);
+}
+
+{
+    const name = 'iPhone 13 Pro',
+        count = 5,
+        category = 'mobile phone',
+        price = 999;
+}

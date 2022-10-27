a4c45de46d9c4e8f8b581d7576fa221b571d59b5
shaman <shaman@shamans-MacBook-Pro.local>
2022-10-26 22:37:43 -0500

    Module 1, lesson 3

---
+++ lesson03/einstein.js
@@ -0,0 +1,8 @@
+'use strict'
+
+{
+    const myWeight = 120,
+    speedLight = 3e8;
+    const E = myWeight * (speedLight ** 2);
+    console.log(`Значение энергии моего тела по формуле E=mc2 равна: +${E}`);
+}

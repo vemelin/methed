b2a2c329da792d6a2ec2c12035bb255d0fa3ecc3
shaman <shaman@shamans-MacBook-Pro.local>
2022-10-27 14:25:09 -0500

    Module 1, lesson 6

---
+++ lesson06/task01.js
@@ -0,0 +1,12 @@
+'use strict'
+
+{
+    for (let i = 1; i <= 10; i++) {
+        console.log(`------------------------------`);
+        for (let y = 1; y <= 10; y++) {
+            console.log(`${i} ^ ${y} = ${i ** y}`);
+        }
+    };
+}
+
+

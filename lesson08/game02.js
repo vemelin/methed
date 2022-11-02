9b502adedb885b5cf7737933c1dd7074e95d0b05
shaman <shaman@shamans-MacBook-Pro.local>
2022-11-01 23:18:52 -0500

    updated syntax

---
+++ lesson08/game02.js
@@ -0,0 +1,31 @@
+'use strict'
+{
+  const isNum = (n) => !isNaN(parseFloat(n)) && isFinite(n);
+  const randomNumber = () => Math.round(Math.random() * 101);
+  const startGame = (input) => {
+    const randomNum = randomNumber();
+    console.log('Random Num: ' + randomNum);
+    let userNum;
+    do {
+      const userNum = +prompt('Enter your number', ``)
+      if (isNaN(parseFloat(userNum)) || userNum === '') {
+        console.log('Введите число');
+        return startGame();
+      } if (userNum === null || userNum === 0) {
+        console.log('Игра закончена'); 
+        break;
+      } if (userNum < randomNum) {
+        console.log('Больше!');
+        return startGame();
+      } if (userNum > randomNum) {
+        console.log('Меньше!');
+        return startGame();
+      } else {
+        console.log('Правильно!');
+        break;
+      }
+    } while (!isNum(userNum));
+    
+  };
+  startGame();
+}

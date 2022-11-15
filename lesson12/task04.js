5fc7819e4623927c60e61951b6babc74ab2a7d97
shaman <shaman@shamans-MacBook-Pro.local>
2022-11-15 08:01:23 -0600

    Module 1, Lesson 13

--- lesson12/task04.js
+++ lesson12/task04.js
@@ -5,15 +5,15 @@
     totalPrice: 0,
     count: 0,
     discount: 0,
-    set setTotalPrice(n) {
-      !Number.isNaN(parseFloat(n)) && isFinite(n) && typeof n !== 'string' ?
-      this.totalPrice = n :
-      this.totalPrice = Math.round(this.calculateItemPrice() * this.discount);
-    },
     set setDiscount(promocode) {
       promocode === 'METHED' ? this.discount = 15 / 100 :
       promocode === 'NEWYEAR' ? this.discount = 21 / 100 :
       this.discount = 0;
+    },
+    set setTotalPrice(n) {
+      !Number.isNaN(parseFloat(n)) && isFinite(n) && typeof n !== 'string' ?
+      this.totalPrice = n :
+      this.totalPrice = Math.round(this.calculateItemPrice() * this.discount);
     },
     isNum(n) {
       return !Number.isNaN(parseFloat(n)) && isFinite(n);

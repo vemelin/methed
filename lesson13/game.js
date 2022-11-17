c819b200f7dde93d452041d2d785aaf232da329d
shaman <shaman@shamans-MacBook-Pro.local>
2022-11-16 20:17:24 -0600

    updated object

--- lesson13/game.js
+++ lesson13/game.js
@@ -4,25 +4,66 @@
     const FIGURES_ENG = ['rock', 'scissors', 'paper'];
     const FIGURES_RUS = ['камень', 'ножницы', 'бумага'];
     const gameProps = {
-      player: 0,
-      computer: 0,
+      user: 0,
+      bot: 0,
+      userInc: 0,
+      botInc: 0,
       language: {
-        'en': ['Player wins!', 'Computer wins!', 'Draw', 'Select', 'User', 'Computer', 'Are you sure?', 'Results'],
-        'ru': ['Вы выиграли', 'Вы проиграли', 'Ничья', 'Выберите', 'Пользователь', 'Компьютер', 'Хотите выйти?', 'Результаты'],
+        'en': [
+          'Player wins!', 'Computer wins!', 'Draw', 'Select',
+          'User', 'Computer', 'Are you sure?', 'Results',
+        ],
+        'ru': [
+          'Вы выиграли', 'Вы проиграли', 'Ничья', 'Выберите',
+          'Пользователь', 'Компьютер', 'Хотите выйти?', 'Результаты',
+        ],
       },
-      getUserAnswer(lang, arr) {
+      getAnswer(lang, arr) {
         const strConfirm = lang[6];
-        const message = `${lang[3]}: ${arr[0]}, ${arr[1]}, ${arr[2]}`;
-        const userMessage = prompt(message, ' ');
-        if (typeof userMessage === 'string' && userMessage !== 'undefined') {
-          const myRe = new RegExp(userMessage?.toLowerCase() || 'undefined', 'y');
-          const userFilter = arr.filter((i) => myRe.exec(i));
-          const botFilter = [...Array(arr)].map((i) => i[this.randomNum(0, 2)]);
-          if (!arr.includes(userFilter[0])) return this.getUserAnswer(lang, arr);
-          return [userFilter[0], botFilter[0]];
+        const m = `${lang[3]}: ${arr[0]}, ${arr[1]}, ${arr[2]}`;
+        const uMSG = prompt(m, ' ');
+        if (typeof uMSG === 'string' && uMSG !== 'undefined') {
+          const myRe = new RegExp(uMSG?.toLowerCase() || 'undefined', 'y');
+          const uFilter = arr.filter((i) => myRe.exec(i));
+          const bFilter = [...Array(arr)].map((i) => i[this.randomNum(0, 2)]);
+          if (!arr.includes(uFilter[0])) return this.getAnswer(lang, arr);
+          return [uFilter[0], bFilter[0]];
         }
-        if (confirm(strConfirm) === false) return this.getUserAnswer(lang, arr);
+        if (confirm(strConfirm) === false) return this.getAnswer(lang, arr);
         return 0;
+      },
+      run(arg) {
+        let arr = []; let state;
+        arg === 'ENG' || arg === 'EN' ? arr = FIGURES_ENG : arr = FIGURES_RUS;
+        arg === 'ENG' || arg === 'EN' ? state = this.language['en'] :
+        state = this.language['ru'];
+        const answer = this.getAnswer(state, arr);
+        const user = answer[0] ?? 0; const bot = answer[1] ?? 0;
+        if (answer === 0) {
+          alert(state[7] + '\n' + state[5] + ': ' +
+          this.bot + '\n' +
+          state[4] + ': ' + this.user);
+          return;
+        }
+        console.log(`You: ${user} Bot: ${bot}`);
+        if ((user === arr[0] && bot === arr[1]) ||
+            (user === arr[2] && bot === arr[0]) ||
+            (user === arr[1] && bot === arr[2])) {
+          this.user = ++this.userInc;
+          alert(state[5] + ': ' + this.bot + '\n' +
+          state[4] + ': ' + this.user + '\n' + state[0]);
+          return this.run(arg);
+        }
+        if (user === bot) {
+          alert(state[5] + ': ' + this.bot + '\n' +
+          state[4] + ': ' + this.user + '\n' + state[2]);
+          return this.run(arg);
+        } else {
+          this.bot = ++this.botInc;
+          alert(state[5] + ': ' + this.bot + '\n' +
+          state[4] + ': ' + this.user + '\n' + state[1]);
+          return this.run(arg);
+        }
       },
       randomNum(min, max) {
         min = Math.ceil(min);
@@ -30,36 +71,10 @@
         return Math.floor(Math.random() * (max - min + 1) + min);
       },
     };
-    let userPoint = 0;
-    let botPoint = 0;
-    const system = (language, user, bot) => {
-      let arr = []; let lang = [];
-      language === 'ENG' || language === 'EN' ? arr = FIGURES_ENG : arr = FIGURES_RUS;
-      language === 'ENG' || language === 'EN' ? lang = gameProps.language.en : lang = gameProps.language.ru;
-      const answer = gameProps.getUserAnswer(lang, arr);
-      user = answer[0] ?? 0; bot = answer[1] ?? 0;
-      if (answer === 0) {
-        alert(`${lang[7]}:\n${lang[5]}: ${botPoint},\n${lang[4]}: ${userPoint}`);
-        return;
-      }
-      console.log(`You: ${user} Bot: ${bot}`);
-      if ((user === arr[0] && bot === arr[1]) || (user === arr[2] && bot === arr[0]) || (user === arr[1] && bot === arr[2])) {
-        alert(`${lang[5]}: ${botPoint},\n${lang[4]}: ${userPoint}\n${lang[0]}:`);
-        gameProps.player = ++userPoint;
-        return system(language);
-      }
-      if (user === bot) {
-        alert(`${lang[5]}: ${botPoint},\n${lang[4]}: ${userPoint}\n${lang[2]}:`);
-        return system(language);
-      } else {
-        gameProps.computer = ++botPoint;
-        alert(`${lang[5]}: ${botPoint},\n${lang[4]}: ${userPoint}\n${lang[1]}:`);
-        return system(language);
-      }
-    };
     const game = (language) => {
-      system(language);
-      console.log(`You: ${gameProps.player} Bot: ${gameProps.computer}`);
+      gameProps.run(language);
+      console.log(`You: ${gameProps.user} Bot: ${gameProps.bot}`);
+      console.log(gameProps);
     };
     return {
       game,
---
+++ lesson13/game_bckp_old_ver.js
@@ -0,0 +1,69 @@
+'use strict';
+{
+  window.start = (() => {
+    const FIGURES_ENG = ['rock', 'scissors', 'paper'];
+    const FIGURES_RUS = ['камень', 'ножницы', 'бумага'];
+    const gameProps = {
+      player: 0,
+      computer: 0,
+      language: {
+        'en': ['Player wins!', 'Computer wins!', 'Draw', 'Select', 'User', 'Computer', 'Are you sure?', 'Results'],
+        'ru': ['Вы выиграли', 'Вы проиграли', 'Ничья', 'Выберите', 'Пользователь', 'Компьютер', 'Хотите выйти?', 'Результаты'],
+      },
+      getUserAnswer(lang, arr) {
+        const strConfirm = lang[6];
+        const message = `${lang[3]}: ${arr[0]}, ${arr[1]}, ${arr[2]}`;
+        const userMessage = prompt(message, ' ');
+        if (typeof userMessage === 'string' && userMessage !== 'undefined') {
+          const myRe = new RegExp(userMessage?.toLowerCase() || 'undefined', 'y');
+          const userFilter = arr.filter((i) => myRe.exec(i));
+          const botFilter = [...Array(arr)].map((i) => i[this.randomNum(0, 2)]);
+          if (!arr.includes(userFilter[0])) return this.getUserAnswer(lang, arr);
+          return [userFilter[0], botFilter[0]];
+        }
+        if (confirm(strConfirm) === false) return this.getUserAnswer(lang, arr);
+        return 0;
+      },
+      randomNum(min, max) {
+        min = Math.ceil(min);
+        max = Math.floor(max);
+        return Math.floor(Math.random() * (max - min + 1) + min);
+      },
+    };
+    let userPoint = 0;
+    let botPoint = 0;
+    const system = (language, user, bot) => {
+      let arr = []; let lang = [];
+      language === 'ENG' || language === 'EN' ? arr = FIGURES_ENG : arr = FIGURES_RUS;
+      language === 'ENG' || language === 'EN' ? lang = gameProps.language.en : lang = gameProps.language.ru;
+      // console.log(gameProps.language['ru'][5]);
+      const answer = gameProps.getUserAnswer(lang, arr);
+      user = answer[0] ?? 0; bot = answer[1] ?? 0;
+      if (answer === 0) {
+        alert(`${lang[7]}:\n${lang[5]}: ${botPoint},\n${lang[4]}: ${userPoint}`);
+        return;
+      }
+      console.log(`You: ${user} Bot: ${bot}`);
+      if ((user === arr[0] && bot === arr[1]) || (user === arr[2] && bot === arr[0]) || (user === arr[1] && bot === arr[2])) {
+        alert(`${lang[5]}: ${botPoint},\n${lang[4]}: ${userPoint}\n${lang[0]}:`);
+        gameProps.player = ++userPoint;
+        return system(language);
+      }
+      if (user === bot) {
+        alert(`${lang[5]}: ${botPoint},\n${lang[4]}: ${userPoint}\n${lang[2]}:`);
+        return system(language);
+      } else {
+        gameProps.computer = ++botPoint;
+        alert(`${lang[5]}: ${botPoint},\n${lang[4]}: ${userPoint}\n${lang[1]}:`);
+        return system(language);
+      }
+    };
+    const game = (language) => {
+      system(language);
+      console.log(`You: ${gameProps.player} Bot: ${gameProps.computer}`);
+    };
+    return {
+      game,
+    };
+  })();
+}

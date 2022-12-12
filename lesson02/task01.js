afbd9fb1e6d8d62626fe5e17359b3f91899799ba
Vasiliy <vemelin@gmail.com>
2022-12-12 16:28:30 -0600

    Task 2 and menu refines

--- lesson02/task01.js
+++ lesson02/task01.js
@@ -9,13 +9,14 @@
   const debounce = (func, time) => {
     return function execute(...args) {
       let previousCall = this.lastCall;
+      this.lastCall = Date.now();
       if (previousCall && this.lastCall - previousCall <= time) {
         clearTimeout(this.lastCall);
       }
       this.lastCallTimer = setTimeout(() => func(...args), time)
     }
   };
-
+  
   const setInputData = () => p.textContent = input.value;
   const debounceHandle = debounce(setInputData, 300);
   input.addEventListener('keyup', debounceHandle);
